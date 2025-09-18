ae0558234b9cbbeace48b554c60fb83378adb70b
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-09-18 12:50:38 -0600

    feat: hola mundo

---
+++ hello_word/target/debug/deps/hello_word-ce11450195bed798.d
@@ -0,0 +1,5 @@
+/home/charizardbellako/Documentos/Rust/Rust_first_steps/hello_word/target/debug/deps/hello_word-ce11450195bed798.d: src/main.rs
+
+/home/charizardbellako/Documentos/Rust/Rust_first_steps/hello_word/target/debug/deps/libhello_word-ce11450195bed798.rmeta: src/main.rs
+
+src/main.rs:
---
+++ hello_word/target/debug/deps/hello_word-f0026586f84e47c2.d
@@ -0,0 +1,5 @@
+/home/charizardbellako/Documentos/Rust/Rust_first_steps/hello_word/target/debug/deps/hello_word-f0026586f84e47c2.d: src/main.rs
+
+/home/charizardbellako/Documentos/Rust/Rust_first_steps/hello_word/target/debug/deps/libhello_word-f0026586f84e47c2.rmeta: src/main.rs
+
+src/main.rs:
