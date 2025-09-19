03328dc1d36ae1c8674777f4d56ad9c99e83f08b
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-09-19 11:33:24 -0600

    feat: recibir el input del usuario

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,10 @@
+use std::io; //Importacion de la libreria de entrada y salida
+fn main() {
+    println!("Guess the number!");
+    println!("Please input your guess.");
+    let mut guess = String::new();// Crea una variable mutable llamada guess que es una cadena de texto vacia
+    io::stdin()// Toma la entrada del usuario desde la consola
+        .read_line(&mut guess)// Lee la linea de entrada y la guarda en la variable guess
+        .expect("Failed to read line");// Si hay un error al leer la linea, imprime el mensaje de error
+    println!("You guessed: {guess}");
+}
