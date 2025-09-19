32e74924b2ec192fab258e49960a4c43515e2284
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-09-19 13:07:42 -0600

    feat: ferris

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,8 +3,9 @@ fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
     let mut guess = String::new();// Crea una variable mutable llamada guess que es una cadena de texto vacia
+    //std::io::stdin se puede utilizar la libreria std:: aun sin importar al principio del codigo
     io::stdin()// Toma la entrada del usuario desde la consola
-        .read_line(&mut guess)// Lee la linea de entrada y la guarda en la variable guess
+        .read_line(&mut guess)// Lee la linea de entrada y la guarda en la variable guess, el & permite que varias partes del corigo accedan a una pieza de datos sin sobreescribirla q 
         .expect("Failed to read line");// Si hay un error al leer la linea, imprime el mensaje de error
     println!("You guessed: {guess}");
 }
