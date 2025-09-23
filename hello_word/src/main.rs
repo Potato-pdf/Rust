32fef93ce3350614adaa2e720b82c271cc48542c
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-09-23 13:39:58 -0600

    feat: operadores aritmeticos

--- hello_word/src/main.rs
+++ hello_word/src/main.rs
@@ -1,5 +1,6 @@
 mod variables;
 mod tipos_de_datos;
+mod operadores;
 
 fn main() {
 	// main es la funcion principal, es donde se ejecuta el codigo de Rust
@@ -7,5 +8,8 @@ fn main() {
 	//tipos_de_datos::escalares::datos_escalares();
     //tipos_de_datos::compuestos::datos_compuestos();
     //variables::variables_y_mutabilidad::variables_y_mutabilidad();
-    variables::shadowing::shadowing();
+    //variables::shadowing::shadowing();
+    operadores::operadores::operadores_aritmeticos();
+    operadores::operadores::operadores_logicos();
+    //operadores::operadores::operadores_de_comparacion();
 }
---
+++ hello_word/src/operadores/operadores.rs
@@ -0,0 +1,21 @@
+pub fn operadores_aritmeticos() {
+    let a: i8 = 10;
+    let b: i8 = 5;
+    print!("Suma: {}", a + b); // Suma
+    print!("Resta: {}", a - b); // Resta
+    print!("Multiplicacion: {}", a * b); // Multiplicacion
+    print!("Division: {}", a / b); // Division, unicamente en enteros
+    print!("Modulo: {}", a % b); // Modulo, unicamente en enteros
+}
+
+pub fn operadores_logicos() {
+    let a: bool = true;
+    let b: bool = false;
+    print!("AND: {}", a && b); // AND
+    print!("OR: {}", a || b); // OR
+    print!("NOT: {}", !a); // NOT
+}
+
+pub fn operadores_de_comparacion() {
+
+}
