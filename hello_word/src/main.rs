59f99627c9d923dc5e2f8b020aea748f59baaf28
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-09-23 12:14:39 -0600

    feat: entendimiento de la diferencia en prints

--- hello_word/src/main.rs
+++ hello_word/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {// main es la funcion principal, es donde se ejecuta el codigo de Rust, los parametros de la funcion main van entre los parentesis
     // Print "Hello, world!" to the console
-    println!("Hello, world!");
+    let hola: &str = "Hola, mundo!";
+    println!("{}", hola);
     // variable 
     let string: &str = "String variable"; // &str es un tipo de dato que representa una cadena de texto que reserva el espacio en memoria
     println!("Variable: {}", string); // Imprime la variable, requiere el uso de {} para indicar que se va a imprimir una variable
@@ -10,6 +11,10 @@ fn main() {// main es la funcion principal, es donde se ejecuta el codigo de Rus
     string_mutable = "Cadena mutable cambiada"; // Cambia el valor de la variable mutable
     println!("Variable mutable despues del cambio: {}", string_mutable); // Imprime la variable mutable despues del cambio
     // string_mutable = 5; // Error, no se puede cambiar el tipo de dato de una variable
+
+    let mut contador: i32 = 0; // i32 es un tipo de dato que representa un entero de 32 bits
+    contador = contador + 1; // Incrementa el valor de la variable contador en 1
+    println!("Contador: {}", contador); // Imprime el valor de la variable contador
 
     let string_string: String = String::from("String variable con String"); // String es un tipo de dato que representa una cadena de texto que no reserva el espacio en memoria, solo usa lo que necesita
     println!("Variable con String: {}", string_string); // Imprime la variable con String
