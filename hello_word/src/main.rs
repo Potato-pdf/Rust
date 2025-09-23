2ffd9216eb34fa5f74a9d90dbb0e9cd4f5fe3ea2
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-09-23 12:48:23 -0600

    feat: pub datos_escalares

--- hello_word/src/main.rs
+++ hello_word/src/main.rs
@@ -3,5 +3,6 @@ mod tipos_de_datos;
 
 fn main() {
 	// main es la funcion principal, es donde se ejecuta el codigo de Rust
-	variables::tipos_variables::variable();
+	//variables::tipos_variables::variable();
+	tipos_de_datos::escalares::datos_escalars();
 }
--- hello_word/src/tipos_de_datos/escalares.rs
+++ hello_word/src/tipos_de_datos/escalares.rs
@@ -1,3 +1,11 @@
-fn datos_escalars() {
+pub fn datos_escalars() {
+    let numero_sin_signo: u32 = 42; // u32 es un tipo de dato que representa un entero sin signo de 32 bits osea del 0 al 4,294,967,295
+    println!("Numero sin signo: {}", numero_sin_signo);
+    let numero_con_signo: i32 = -42; // i32 es un tipo de dato que representa un entero con signo de 32 bits osea del -2,147,483,648 al 2,147,483,647
+    println!("Numero con signo: {}", numero_con_signo);
+    let numero_flotante: f32 = 3.14; // f32 es un tipo de dato que representa un numero de punto flotante de 32 bits representa numeros con decimales
+    println!("Numero flotante: {}", numero_flotante);
+    let booleano: bool = true; // bool es un tipo de dato que representa un valor booleano, puede ser true o false
+    println!("Booleano: {}", booleano);
 
 }
