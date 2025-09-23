0571de9b5488b9c8c804421e7837342463404542
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-09-23 13:15:57 -0600

    feat: constantes

--- hello_word/src/variables/variables_y_mutabilidad.rs
+++ hello_word/src/variables/variables_y_mutabilidad.rs
@@ -1,8 +1,12 @@
 pub fn variables_y_mutabilidad() {
     let numero_inmutable: i32 = 43; // Variable inmutable, let es la palabra reservada para declarar variables
     println!("Numero inmutable: {}", numero_inmutable);
+
     let mut numero_mutables: i32 = 44; // Variable mutable, mut es la palabra reservada para convertir una variable en mutable
     println!("Numero mutable antes del cambio: {}", numero_mutables);
     numero_mutables = 45;
     println!("Numero mutable después del cambio: {}", numero_mutables);
+
+    const CONSTANTE: i32 = 100; // Constante, const es la palabra reservada para declarar constantes, las constantes deben tener un tipo de dato y no pueden cambiar su valor
+    println!("Constante: {}", CONSTANTE);
 }
