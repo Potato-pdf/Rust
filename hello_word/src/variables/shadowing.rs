2e9b9670e4b0a096734a9ee485b5493d10cc1bd5
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-09-23 13:30:43 -0600

    feat: ejemplo shaowing

--- hello_word/src/variables/shadowing.rs
+++ hello_word/src/variables/shadowing.rs
@@ -1,3 +1,5 @@
 pub fn shadowing() {
-    
+    let numero: i128 = 5;
+    let numero: i128 = numero + 1; // Sombreamos la variable numero, ahora numero es 6
+    print!("Numero sombreador: {}", numero);// sombrear una variable, quiere decir declarar una nueva variable con el mismo nombre que una variable anterior, la nueva variable "sombrea" a la anterior
 }
