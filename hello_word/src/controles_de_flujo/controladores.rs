8a776b34f1a1ac6dd49a159ee03028aa59aab37f
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-09-24 09:46:08 -0600

    feat: else, if. elif

--- hello_word/src/controles_de_flujo/controladores.rs
+++ hello_word/src/controles_de_flujo/controladores.rs
@@ -1,4 +1,30 @@
-pub fn if_example() {}
-pub fn else_example() {}
-pub fn else_if_example() {}
-pub fn match_example() {}
+pub fn if_example() {
+        let numero: i8 = 5;
+        if numero < 10 {
+            println!("El número es menor que 10");
+        }
+}
+pub fn else_example() {
+    let numero: i8 = 5;
+
+    if numero < 10 {
+        println!("El número es menor que 10");
+    } else {
+        println!("El número es mayor o igual que 10");
+    }
+}
+pub fn else_if_example() {
+    let numero: i8 = 5;
+
+    if numero < 10 {
+        println!("El número es menor que 10");
+    } else if numero == 10 {
+        println!("El número es igual a 10");
+    } else {
+        println!("El número es mayor que 10");
+    }
+}
+pub fn match_example() {
+    let numero: i8 = 5;
+
+}
