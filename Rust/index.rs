1da718a575ddffc41c91126a7afc258d329d2cb6
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2026-03-12 15:05:11 -0600

    feat: refact proyect

---
+++ Rust/index.rs
@@ -0,0 +1,6 @@
+fn main() {
+    /// Println! no es una funcion, es un macro
+    /// Los macros en Rust se invocan con un signo de exclamación `!`
+    /// Estos generan codigo antes de compilar lo que permite cosas que las funciones normales no pueden
+    println!("Hello, world!");
+}
---
+++ Rust/rust.md
@@ -0,0 +1,3 @@
+Compílalo manualmente ejecutando en tu terminal: rustc nombre_archivo.rs
+
+Verás que se generó un archivo binario llamado hola (sin extensión). Ejecútalo con: ./hola
