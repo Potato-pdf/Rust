bcd6a39263cf2945eba4b0f0b27c7058d3d8b0e0
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2026-03-18 14:56:29 -0600

    feat: expresiones y sentencias

---
+++ Rust/rust-proyect/src/expresiones/expresiones.rs
@@ -0,0 +1,10 @@
+/// las expresiones en Rust son valores que se evalúan y producen un resultado
+/// Una expresión puede ser una operación matemática, una llamada a función, o incluso un bloque de código.
+
+pub fn expresiones() {
+    println!("Las expresiones en Rust son valores que se evalúan y producen un resultado.");
+}
+
+/// Las sentencias en Rust son instrucciones que realizan una acción pero no producen un valor.
+pub fn sentencia()  {
+ii
