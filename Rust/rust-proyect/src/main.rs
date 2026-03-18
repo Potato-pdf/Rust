741519381fb9bd1b0adf722e6a346c49b8b67815
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2026-03-18 14:56:18 -0600

    feat: expessions main

--- Rust/rust-proyect/src/main.rs
+++ Rust/rust-proyect/src/main.rs
@@ -1,3 +1,16 @@
+mod funciones;
+use funciones::*;
+mod expresiones;
+use expresiones::*;
+
 fn main() {
-    println!("Hello, world!");
+    saludar();
+    let resultado = sumar(5, 3);
+    println!("El resultado de la suma es: {}", resultado);
+
+    let resultado_resta = restar(10, 4);
+    println!("El resultado de la resta es: {}", resultado_resta);
+
+    expresiones();
+
 }
