15b9ca91ccc3580250b86a5b8fe6ef9486553055
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2026-03-24 13:58:20 -0600

    feat: delete un use method

--- Rust_library/src/lib.rs
+++ Rust_library/src/lib.rs
@@ -1,24 +1,28 @@
 use std::ffi::CString;
 use std::os::raw::c_char;
 
-/**
-NO mangle inhibits the name mangling of the function.
-This is necessary for the function to be called from C.
-**/
-#[unsafe(no_mangle)]
-
-pub extern "C" fn mudra_init_handshake() -> *mut c_char {
-    let message = "Mudra Core v0.1.0";
-    /*
-    Convertimos un String de Rust a un CString (compatible con C/Bun)
-      */
-    let c_message = CString::new(message).unwrap();
-    /*
-    into_raw() convierte el CString en un puntero crudo a c_char
-    Trasnfiere la propiedad de la memoria a el que lo reciba
-    */
-    c_message.into_raw()
+/** 
+Structura de prueba privada para proyectos fuera de Rust_library.
+*/
+pub struct TestStruct {
+    id: u32,
+    request_count: u32,
 }
+/**
+Constructor de prueba para TestStruct.
+Crea una nueva instancia de TestStruct con el id proporcionado y un request_count inicial de 0.
+Utilizamos "Box::" para asignar la estructura en el heap, lo que permite que su memoria persista después de que la función termine.
+con "Box::into_raw" convertimos el Box en un puntero crudo, transfiriendo la propiedad de la memoria al llamador (en este caso, Bun).
+*/
+#[unsafe(no_mangle)]
+pub extern "C" fn test_constructor(id: u32) -> *mut TestStruct {
+    let test_sdk = Box::new(TestStruct{
+        id,
+        request_count: 0,
+    });
+    Box::into_raw(test_sdk)
+}
+
 
 #[unsafe(no_mangle)]
 pub extern "C" fn mudra_free_string(s: *mut c_char) {
@@ -26,6 +30,6 @@ pub extern "C" fn mudra_free_string(s: *mut c_char) {
         if s.is_null() {
             return;
         }
-        CString::from_raw(s); // Libera la memoria
+        CString::from_raw(s); 
     }
 }
